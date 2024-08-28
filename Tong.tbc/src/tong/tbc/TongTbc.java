fd1b7ec1ab5667177d87ab1e59028652fd1beb96
NhattHuyy <149464989+huyvpip@users.noreply.github.com>
2024-08-28 16:39:57 +0700

    Add files via upload

---
+++ Tong.tbc/src/tong/tbc/TongTbc.java
@@ -0,0 +1,32 @@
+package tong.tbc;
+
+import java.util.Scanner;
+
+public class TongTbc {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhap so luong phan tu mang: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Nhap cac phan tu cua mang:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Phan tu thu " + (i + 1) + ": ");
+            arr[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        double average = (double) sum / n;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("AGV: " + average);
+    }
+}
